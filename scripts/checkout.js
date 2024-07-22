7eba5a2207fa37f6dd9a791972c3b53747a9ccdd
Your Name <you@example.com>
2024-07-22 18:12:27 +0500

    save cart in local Storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -23,7 +23,7 @@ products.forEach((product) => {
 
 
 
-cartSummaryHtml += `<div class="cart-item-container">
+cartSummaryHtml += `<div class="cart-item-container, js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -111,7 +111,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', ()=> {
     const productId = link.dataset.productId
     removeFromCart(productId)
-    console.log(cart)
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`)  
+
+    container.remove()
     
   })
 
